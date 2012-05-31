0ca08d6f74c7fd955605d243c277ab6efe0f457c
Jonathan Eads <superjeads@gmail.com>
2012-05-31 11:56:40 -0700

    made options key optional

--- datazilla/model/models.py
+++ datazilla/model/models.py
@@ -366,7 +366,6 @@ class DatazillaModel(object):
         self._setTestAuxData(data, refData)
         self._setTestData(jsonData, refData)
 
-
     def _setTestData(self, jsonData, refData):
 
         self.sources["perftest"].set_data('set_test_data',
@@ -555,12 +554,13 @@ class DatazillaModel(object):
     def _getOptionIds(self, data, refData):
         optionIds = dict()
         try:
-            for option in data['testrun']['options']:
-                if option in refData['options']:
-                    optionIds[ option ] = refData['options'][option]
-                else:
-                    testId = self.sources["perftest"].set_data_and_get_id('set_option_data', [ option ])
-                    optionIds[ option ] = testId
+            if 'options' in data['testrun']:
+                for option in data['testrun']['options']:
+                    if option in refData['options']:
+                        optionIds[ option ] = refData['options'][option]
+                    else:
+                        testId = self.sources["perftest"].set_data_and_get_id('set_option_data', [ option ])
+                        optionIds[ option ] = testId
         except KeyError:
             raise
         else:
