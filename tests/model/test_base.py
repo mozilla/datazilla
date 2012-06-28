304d197537481c8f3bd9609e75a6628bc6196f1c
Carl Meyer <carl@oddbird.net>
2012-06-27 20:30:34 -0600

    Tests for DatazillaModel._set_test_values

--- tests/model/test_base.py
+++ tests/model/test_base.py
@@ -378,6 +378,40 @@ def test_set_option_data(dm):
     assert row_data['value'] == 'val'
 
 
+def test_set_test_values(dm):
+    """Inserts test results in the db."""
+    data = TestData(perftest_data(results={"example.com": [1, 2, 3]}))
+
+    # Create all the prerequisites for getting a test_run_id
+    test_id = dm._get_or_create_test_id(data)
+    os_id = dm._get_or_create_os_id(data)
+    product_id = dm._get_or_create_product_id(data)
+    machine_id = dm._get_or_create_machine_id(data)
+
+    build_id = dm._set_build_data(data, os_id, product_id, machine_id)
+
+    test_run_id = dm._set_test_run_data(data, test_id, build_id)
+
+    # Try to set the test values
+    dm._set_test_values(data, test_id, test_run_id)
+
+    page_row = dm.sources["perftest"].dhub.execute(
+        proc="perftest_test.selects.pages",
+        placeholders=[test_id],
+        )[0]
+
+    value_rows = dm.sources["perftest"].dhub.execute(
+        proc="perftest_test.selects.test_values",
+        placeholders=[test_id],
+        )
+    enumerated_values = set([(r['run_id'], r['value']) for r in value_rows])
+
+
+    assert page_row['url'] == 'example.com'
+    assert enumerated_values == set([(1, 1), (2, 2), (3, 3)])
+    assert set([r['page_id'] for r in value_rows]) == set([page_row['id']])
+
+
 
 # TODO fill in the following tests:
 
