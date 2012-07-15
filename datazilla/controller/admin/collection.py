b3efd2f95c3c01605ac4f715df444ee8c2ee1ff6
Jonathan Eads <superjeads@gmail.com>
2012-07-15 15:23:46 -0700

    fixed test_collection population strategy to avoid failed inserts

--- datazilla/controller/admin/collection.py
+++ datazilla/controller/admin/collection.py
@@ -2,26 +2,45 @@
 Functions for populating test collections.
 
 """
+from MySQLdb import IntegrityError
+
 from datazilla.model import PerformanceTestModel
 
 
 def load_test_collection(project):
 
-    dm = PerformanceTestModel(project)
+    ptm = PerformanceTestModel(project)
 
-    products = dm.get_products('id')
+    products = ptm.get_products('id')
 
-    for product_name in products:
+    product_names = {}
 
-        if products[ product_name ]['product'] and \
-           products[ product_name ]['version'] and \
-           products[ product_name ]['branch']:
+    for id in products:
 
-            name = "%s %s %s" % (products[ product_name ]['product'],
-                                 products[ product_name ]['version'],
-                                 products[ product_name ]['branch'])
+        if products[ id ]['product'] and \
+           products[ id ]['version'] and \
+           products[ id ]['branch']:
 
-            id = dm.set_test_collection(name, "")
-            dm.set_test_collection_map(id, products[ product_name ]['id'])
+            name = get_test_collection_name(
+                products[ id ]['product'],
+                products[ id ]['version'],
+                products[ id ]['branch']
+                )
 
-    dm.disconnect()
+            product_names[name] = id
+
+    test_collection_names = ptm.get_test_collection_set()
+
+    new_name_set = set(
+        product_names.keys()
+        ).difference( test_collection_names )
+
+    for new_name in new_name_set:
+        id = ptm.set_test_collection(new_name, "")
+        ptm.set_test_collection_map(id, product_names[ new_name ])
+
+    ptm.disconnect()
+
+def get_test_collection_name(product, version, branch):
+    name = "%s %s %s" % (product, version, branch)
+    return name
