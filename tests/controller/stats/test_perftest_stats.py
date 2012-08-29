05807fa1a56aac22d6ba14954133f8d23cb4f87f
Cameron Dawson <cdawson@mozilla.com>
2012-08-29 15:15:31 -0700

    working on a test

--- tests/controller/stats/test_perftest_stats.py
+++ tests/controller/stats/test_perftest_stats.py
@@ -1,7 +1,9 @@
 from ...sample_data import perftest_json
 from datazilla.controller.admin.stats import perftest_stats
 
-def test_get_runs_by_branch(ptsm, monkeypatch):
+import json
+
+def test_get_runs_by_branch(ptm, ptsm, monkeypatch):
 
     def mock_ptsm(project):
         return ptsm
@@ -23,11 +25,11 @@ def test_get_runs_by_branch(ptsm, monkeypatch):
         ]
 
     for blob in blobs:
-        ptsm.store_test_data(blob)
-    ptsm.process_objects(3)
+        ptm.store_test_data(blob)
+    ptm.process_objects(3)
 
-    runs = perftest_stats.get_runs_by_branch("talos", 1330454756, 1660454756)
-    assert False, runs
+    runs = perftest_stats.get_runs_by_branch("talos", 1330454757, 1660454757)
+    assert False, json.dumps(runs, indent=4)
 
 
 def test_get_ref_data():
