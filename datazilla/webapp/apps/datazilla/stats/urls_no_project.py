09a2491af2073e5c878bfa0be89f378fc19a6332
Cameron Dawson <cdawson@mozilla.com>
2012-09-04 15:08:08 -0700

    added optional trailing slash without redirect

--- datazilla/webapp/apps/datazilla/stats/urls_no_project.py
+++ datazilla/webapp/apps/datazilla/stats/urls_no_project.py
@@ -4,8 +4,8 @@ urlpatterns = patterns(
     "datazilla.webapp.apps.datazilla.stats",
 
     # pushlog
-    (r"^pushlog/branches/$", "pushlog_views.get_all_branches"),
+    (r"^pushlog/branches/?$", "pushlog_views.get_all_branches"),
 
-    (r"^pushlog/db_size/$", "pushlog_views.get_db_size"),
+    (r"^pushlog/db_size/?$", "pushlog_views.get_db_size"),
 
     )
--- datazilla/webapp/apps/datazilla/urls.py
+++ datazilla/webapp/apps/datazilla/urls.py
@@ -7,13 +7,13 @@ urlpatterns = patterns('',
                        (r'^$', views.graphs),
 
                        #UI Application help
-                       (r'^help$', views.get_help),
+                       (r'^help/?$', views.get_help),
 
                        #Loads JSON object into objectstore
-                       (r'^api/load_test$', views.set_test_data),
+                       (r'^api/load_test/?$', views.set_test_data),
 
                        #Web service methods used by the UI
-                       (r'^api/(?P<method>\w+)$', views.dataview),
+                       (r'^api/(?P<method>\w+)/?$', views.dataview),
 
                        #return statistics about Datazilla
                        (r'^stats/', include("datazilla.webapp.apps.datazilla.stats.urls")),
