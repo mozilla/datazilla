55f7126dcd8726f05ed270d52c40b0e84cdc92c8
Jonathan Eads <superjeads@gmail.com>
2014-06-26 12:01:16 -0700

    set flame as default device

--- datazilla/webapp/static/js/b2g_apps/AppsPage.js
+++ datazilla/webapp/static/js/b2g_apps/AppsPage.js
@@ -186,15 +186,9 @@ var AppsPage = new Class( {
 
         this.urlBase = urlObj.attr.base + urlObj.attr.directory;
 
-        var deviceDefault = 'hamachi';
-        // Device default is project specific
-        if (this.refData.project == 'b2gtw'){
-            deviceDefault = 'tarako';
-        }
-
         this.defaults = {};
         this.defaults['branch'] = urlObj.param.query.branch;
-        this.defaults['device'] = urlObj.param.query.device || deviceDefault;
+        this.defaults['device'] = urlObj.param.query.device || 'flame';
         this.defaults['range'] = urlObj.param.query.range;
         this.defaults['test'] = urlObj.param.query.test || 'cold_load_time';
         this.defaults['app'] = urlObj.param.query.app;
